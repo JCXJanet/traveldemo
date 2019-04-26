772dfec1e9cf943dcaa7cd6334d81a8f8a0edf78
jinchengxiao <chengxiao0720@gmail.com>
2019-04-26 13:08:49 +0800

    use vuex

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -9,7 +9,7 @@
         </div>
         <router-link to="/city">
             <div class="header-right">
-                {{this.city}}
+                {{this.doubleCity}}
                 <span class="iconfont arrow-icon">&#xe64a;</span>
             </div>
         </router-link>
@@ -17,10 +17,12 @@
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex'
 export default {
   name: 'HomeHeader',
-  props: {
-    city: String
+  computed: {
+    ...mapState(['city']),
+    ...mapGetters(['doubleCity'])
   }
 }
 </script>
@@ -30,32 +32,38 @@ export default {
     /*@import "~styles\varibles.styl"*/
     /*1rem = html font-size = 50px*/
     .header
-        display : flex
-        line-height :  $headerHeight
-        background : $bgColor
-        color : #fff
+        display: flex
+        line-height: $headerHeight
+        background: $bgColor
+        color: #fff
+
         .header-left
-          width : .64rem
-          float : left
-          .back-icon
-            text-align : center
-            font-size : .4rem
+            width: .64rem
+            float: left
+
+            .back-icon
+                text-align: center
+                font-size: .4rem
+
         .header-input
-          flex : 1
-          height : .64rem
-          line-height : .64rem
-          margin-top : .12rem
-          margin-left : .2rem
-          padding-left : .2rem
-          background : #fff
-          border-radius : .1rem
-          color : #ccc
+            flex: 1
+            height: .64rem
+            line-height: .64rem
+            margin-top: .12rem
+            margin-left: .2rem
+            padding-left: .2rem
+            background: #fff
+            border-radius: .1rem
+            color: #ccc
+
         .header-right
-          width : 1.24rem
-          float : right
-          text-align : center
-          color: #fff
-          .arrow-icon
-            margin-left : -.04rem
-            font-size : .24rem
+            min-width: 1.04rem
+            padding: 0 .1rem
+            float: right
+            text-align: center
+            color: #fff
+
+            .arrow-icon
+                margin-left: -.04rem
+                font-size: .24rem
 </style>
